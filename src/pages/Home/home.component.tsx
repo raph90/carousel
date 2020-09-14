69c201a2ac70ab23ce2365c42542aa9aeba5bb89
Raphael Hetherington <raph37heth@gmail.com>
2020-09-14 15:34:18 +0100

    added pagination on carousel

--- src/pages/Home/home.component.tsx
+++ src/pages/Home/home.component.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from "react";
+import Carousel from "./relatedComponents/Carousel/carousel.component";
 
 const Home: FC = () => {
-  return <h1>Im home</h1>;
+  return (
+    <div className="home">
+      <Carousel />
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/pages/Home/relatedComponents/Carousel/carousel.component.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from "react";
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import "swiper/components/scrollbar/scrollbar.scss";
+import "./carousel.styles.scss";
+
+SwiperCore.use([Pagination, A11y]);
+const Carousel: FC = () => {
+  return (
+    <div className="slide-container">
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
+        <SwiperSlide>
+          <div className="slide" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Carousel;
