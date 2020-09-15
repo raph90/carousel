6020c8e090d3812f032cb95f9821dc08c86ca039
Raphael Hetherington <raph37heth@gmail.com>
2020-09-15 12:29:52 +0100

    started work on text sections

--- src/pages/Home/home.component.tsx
+++ src/pages/Home/home.component.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from "react";
 import Carousel from "./relatedComponents/Carousel/carousel.component";
 import HeroImageSection from "./relatedComponents/HeroImageSection/heroImageSection.component";
+import TextSectionTop from "./relatedComponents/TextSectionTop/TextSectionTop.component";
 
 const Home: FC = () => {
   return (
     <div className="home">
       <Carousel />
+      <TextSectionTop />
       <HeroImageSection />
     </div>
   );
---
+++ src/pages/Home/relatedComponents/TextSectionTop/TextSectionTop.component.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MainContainer from "../../../../LayoutComponents/MainContainer/main-container.component";
+
+const TextSectionTop: React.FC = () => {
+  return (
+    <MainContainer>
+      <div className="hello">hi</div>
+      <div className="there">there</div>
+    </MainContainer>
+  );
+};
+
+export default TextSectionTop;
